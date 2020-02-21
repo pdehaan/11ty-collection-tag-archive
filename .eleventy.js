e5d25b4cfc4682c212ee158323a6a05399c9f189
Peter deHaan <peter@deseloper.com>
2020-02-20 23:32:28 -0800

    More code, worse code

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,17 @@
 module.exports = eleventyConfig => {
   eleventyConfig.addLiquidFilter("dump", require("util").inspect);
+  // Usage: `{%- assign $title = title | or: renderData.title, site.title -%}`
+  eleventyConfig.addLiquidFilter("or", (value="", ...values) => {
+    // prepend the `value` value at the front of the `values[]` array.
+    values.unshift(value);
+    return values.find(truthy);
+  });
+
+  eleventyConfig.addLiquidFilter("truthy", truthy);
+  eleventyConfig.addLiquidFilter("falsy", value => !truthy(value));
+  eleventyConfig.addLiquidFilter("Boolean", Boolean);
+
+  eleventyConfig.addLiquidFilter("check", value => value ? "✔️" : "&nbsp;");
 
   eleventyConfig.setDataDeepMerge(true);
   eleventyConfig.setQuietMode(true);
@@ -11,3 +23,8 @@ module.exports = eleventyConfig => {
     }
   };
 };
+
+function truthy(value) {
+  if (value === 0) return true;
+  return Boolean(value);
+}
